340d4e625db7ae571f4dc0aa8815a9f0a33995a6
luketaylor9000 <lukeandrewtaylor.lt@gmail.com>
2022-08-24 15:40:10 -0400

    created validations for brewery

--- app/models/brewery.rb
+++ app/models/brewery.rb
@@ -1,3 +1,6 @@
 class Brewery < ApplicationRecord
-
+  validates :obdb_id, presence: true
+  validates :name, presence: true
+  validates :type, presence: true
+  validates :address, presence: true
 end
