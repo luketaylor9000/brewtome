fe732d07e9293f02cda82b71ba41f3257b4ec8c5
luketaylor9000 <lukeandrewtaylor.lt@gmail.com>
2022-08-24 17:11:52 -0400

    set up devise authentication

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_24_192750) do
+ActiveRecord::Schema.define(version: 2022_08_24_204422) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,18 @@ ActiveRecord::Schema.define(version: 2022_08_24_192750) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "reviews", force: :cascade do |t|
+    t.string "rating", null: false
+    t.string "title", null: false
+    t.text "body", null: false
+    t.bigint "brewery_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["brewery_id"], name: "index_reviews_on_brewery_id"
+    t.index ["user_id"], name: "index_reviews_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -35,6 +47,7 @@ ActiveRecord::Schema.define(version: 2022_08_24_192750) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "role", default: "member", null: false
+    t.string "username"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
