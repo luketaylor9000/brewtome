04068642abc88543c08498ce104e7af6e21ec8fe
luketaylor9000 <lukeandrewtaylor.lt@gmail.com>
2022-08-24 15:18:48 -0400

    Notes on checking if brewery exists before show

--- app/controllers/api/v1/breweries_controller.rb
+++ app/controllers/api/v1/breweries_controller.rb
@@ -18,4 +18,23 @@ class Api::V1::BreweriesController < ApiController
     render json: response
   end
 
+  #CHECK IF BREWERY ALREADY EXISTS BEFORE GOING TO NEW BREWERY SHOW PAGE
+  #IF EXISTS THEN RENDER THE EXISTING SHOW PAGE FOR THAT BREWERY
+  
+  # def show 
+  #   brewery = params[:id]
+  #   found_brewery = Brewery.find_by(open_brewery_id: brewery)
+
+  #   if found_brewery.blank?
+  #     response = BreweriesService.get_brewery(brewery)
+  #     render json: response
+  #   else 
+  #     render json: found_brewery
+  #     # note: make brewery serializer later
+  #   end
+    
+  # end
+
+  # # Review.create(...., user: current_user)
+
 end
