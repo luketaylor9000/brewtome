6d990ebc404efd0cd966d78e5be8bd5f41ee12ca
luketaylor9000 <lukeandrewtaylor.lt@gmail.com>
2022-08-25 19:31:21 -0400

    Made some css cards for the BreweryTiles

--- app/javascript/react/components/Breweries/BreweryTile.js
+++ app/javascript/react/components/Breweries/BreweryTile.js
@@ -4,11 +4,18 @@ import { Link } from "react-router-dom"
 const BreweryTile = (props) => {
 
   return (
-    <div className="grid-x small-3">
-        <div className="brewery-tile">
-          <div className="brewery-tile-text">
+    <div className="brewery-tile-container grid-x">
+        <div className="brewery-tile card small-4">
+          <div className="brewery-tile-title card-divider">
             <Link to={`/breweries/${props.id}`}>{props.name}</Link>
           </div>
+          <div className="brewery-tile-photo card-divider">
+            <img className="brewery-tile-img"></img>
+          </div>
+          <div className="brewery-tile-text card-divider">
+            <p>Most recent review tidbit here...</p>
+          </div>
+          <p>posted by User...</p>
         </div>
     </div>
   )
