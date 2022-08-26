805800b9c9e9e296038127c1d48705da07aeea74
luketaylor9000 <lukeandrewtaylor.lt@gmail.com>
2022-08-26 11:44:32 -0400

    added placeholder photo to see how tiles fit

--- app/javascript/react/components/Breweries/BreweryTile.js
+++ app/javascript/react/components/Breweries/BreweryTile.js
@@ -10,7 +10,7 @@ const BreweryTile = (props) => {
             <Link to={`/breweries/${props.id}`}>{props.name}</Link>
           </div>
           <div className="brewery-tile-photo card-divider">
-            <img className="brewery-tile-img"></img>
+            <img className="brewery-tile-img" src="https://cdn.shopify.com/s/files/1/0606/6146/5304/files/Fort_Point_web_first_floor_720x.jpg?v=1647981045" alt="brewery photo"></img>
           </div>
           <div className="brewery-tile-text card-divider">
             <p>Most recent review tidbit here...</p>
