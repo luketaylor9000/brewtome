27b75b1c28c5d768c2e9b3d32712b2f220024841
luketaylor9000 <lukeandrewtaylor.lt@gmail.com>
2022-08-30 11:24:22 -0400

    Removed if logic from breweriesArray

--- app/javascript/react/components/Reviews/ReviewContainer.js
+++ app/javascript/react/components/Reviews/ReviewContainer.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import ReviewTile from "./ReviewTile";
 import moment from "moment";
 import ReviewForm from "./ReviewForm";
@@ -7,23 +7,21 @@ const ReviewContainer = (props) => {
 
   let reviewsArray = props.reviews
 
-  if (reviewsArray !== undefined) {
-    reviewsArray = props.reviews.map((review) => {
-      return (
-        <ReviewTile
-          key={review.id}
-          title={review.title}
-          rating={review.rating}
-          body={review.body}
-          date={moment(review.created_at).format("LL")}
-          breweryId={review.brewery_id}
-          reviewId={review.id}
-          userId={review.user_id}
-          username={review.username}
-        />
-      );
-    });
-  }
+  reviewsArray = props.reviews.map((review) => {
+    return (
+      <ReviewTile
+        key={review.id}
+        title={review.title}
+        rating={review.rating}
+        body={review.body}
+        date={moment(review.created_at).format("LL")}
+        breweryId={review.brewery_id}
+        reviewId={review.id}
+        userId={review.user_id}
+        username={review.username}
+      />
+    );
+  });
 
   return (
     <div>
