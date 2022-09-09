578918471a6647bcb46520bedc91a2b1032e8a85
luketaylor9000 <lukeandrewtaylor.lt@gmail.com>
2022-09-09 17:09:46 -0400

    Deleted old code

--- app/javascript/react/components/Breweries/BreweryTile.js
+++ app/javascript/react/components/Breweries/BreweryTile.js
@@ -3,27 +3,6 @@ import { Link } from "react-router-dom"
 
 const BreweryTile = (props) => {
 
-  // return (
-  //   <div className="brewery-tile-container row">
-  //       <div className="brewery-tile small-4 columns">
-  //       <Link to={`/breweries/${props.id}`}>
-  //         <div className="brewery-tile-title card-divider">
-  //           {props.name}
-  //         </div>
-  //         <div className="brewery-tile-photo card-divider">
-  //           <img className="brewery-tile-img" src="https://cdn.shopify.com/s/files/1/0606/6146/5304/files/Fort_Point_web_first_floor_720x.jpg?v=1647981045" alt="brewery photo"></img>
-  //         </div>
-  //         <div className="brewery-tile-text card-divider">
-  //           <p>Most recent review tidbit here...</p>
-  //         </div>
-  //         <div className="brewery-tile-user card-divider">
-  //           <p>posted by User...</p>
-  //         </div>
-  //       </Link>
-  //       </div>
-  //   </div>
-  // )
-
   return (
     <div className="brewery-tile-container row">
       <div className="brewery-tile small-4 columns">
