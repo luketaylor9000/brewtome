8578d6210562e26e7c09a481f1f631b03d5a431c
luketaylor9000 <lukeandrewtaylor.lt@gmail.com>
2022-09-16 12:57:54 -0400

    Responsive font styling for brewery_top_section

--- app/javascript/react/components/Breweries/BreweryTopSection.js
+++ app/javascript/react/components/Breweries/BreweryTopSection.js
@@ -3,12 +3,17 @@ import React from "react";
 const BreweryTopSection = (props) => {
   return (
     <div className="brewery-top-section">
+        <div className="brewery-banner-photo">
+          <img src="https://www.bigoysterbrewery.com/images/flic/galleries/9/original/29_big-oyster-bar.jpg" alt="brewery banner background" />
+        </div>
       <div className="brewery-top-section-box">
-        <div className="brewery-name">{props.name}</div>
-        <div className="brewery-type">Brewery Type: {`${props.type}`.toUpperCase()}</div>
-        <div className="brewery-address">{props.address}</div>
-        <div className="brewery-website"><a href={props.website}>{props.website}</a></div>
-        <div className="brewery-phone">{props.phone}</div>
+        <div className="brewery-info">
+          <div className="brewery-name">{props.name}</div>
+          <div className="brewery-address">{props.address}</div>
+          <div className="brewery-type">Brewery Type: {`${props.type}`.toUpperCase()}</div>
+          <div className="brewery-website"><a href={props.website}>{props.website}</a></div>
+          <div className="brewery-phone">{props.phone}</div>
+        </div>
       </div>
     </div>
   );
