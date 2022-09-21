87dbe3ae9a0a2b993e3d02efc477d3ec2d576e2e
luketaylor9000 <lukeandrewtaylor.lt@gmail.com>
2022-09-21 11:01:41 -0400

    Styled brewery tiles to be square

--- app/javascript/react/components/Breweries/BreweryTile.js
+++ app/javascript/react/components/Breweries/BreweryTile.js
@@ -8,7 +8,9 @@ const BreweryTile = (props) => {
       <div className="brewery-tile small-4 columns">
       <Link to={`/breweries/${props.id}`}>
           <figure className="image-block">
-            <img src="https://img.freepik.com/free-photo/side-view-various-salty-beer-snacks-wood-platter-pouring-beer-into-mug-rustic-wood_141793-6568.jpg" alt="" />
+            {/* <img src="https://img.freepik.com/free-photo/side-view-various-salty-beer-snacks-wood-platter-pouring-beer-into-mug-rustic-wood_141793-6568.jpg" alt="" /> */}
+            <img src={`https://logo.clearbit.com/${props.website}?size=260`} alt={`${props.name} logo`} />
+            {/* <img src={logo} alt="" /> */}
             <figcaption>
               <h3>{props.name}</h3>
               <p>Most recent review tidbit here...</p>
