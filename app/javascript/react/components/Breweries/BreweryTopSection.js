7af0fe3c5b9d1b1d1ffab39c24b944b6ff34bb93
luketaylor9000 <lukeandrewtaylor.lt@gmail.com>
2022-09-20 23:15:47 -0400

    Front End Logo Disp &Styling for circular logo sho

--- app/javascript/react/components/Breweries/BreweryTopSection.js
+++ app/javascript/react/components/Breweries/BreweryTopSection.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const BreweryTopSection = (props) => {
-
+// debugger
   return (
     <div className="brewery-top-section">
         <div className="brewery-banner-photo">
@@ -9,16 +9,22 @@ const BreweryTopSection = (props) => {
         </div>
       <div className="brewery-top-section-box">
         <div className="brewery-info">
+
+          <div className="brewery-logo">
+            <img className="clearbit-img" src={`https://logo.clearbit.com/${props.website}?size=150`} alt="brewery logo"/>
+          </div>
+
+        {/* <img className="brewery_logo" src={`../images/brewery_logos/${props.obdbid}_logo.png`} alt="brewery logo"/> */}
+        {/* <img className="brewery_logo" src={`../../../../assets/images/brewery_logos/${props.obdbid}_logo.png`} alt="brewery logo"/> */}
+        {/* <img className="brewery_logo" src={`app/assets/images/brewery_logos/${props.id}_logo.png`} alt="brewery logo"/> */}
+        {/* <img className="brewery_logo" src={`../../../../assets/images/brewery_logos/${props.id}_logo.png`} alt="brewery logo"/> */}
+        {/* <img className="brewery_logo" src={require(`../../../../assets/images/brewery_logos/${props.id}_logo.png`)} alt="brewery logo"/> */}
+
           <div className="brewery-name">{props.name}</div>
           <div className="brewery-address">{props.address}</div>
           <div className="brewery-type">Brewery Type: {`${props.type}`.toUpperCase()}</div>
           <div className="brewery-website"><a href={props.website}>{props.website}</a></div>
           <div className="brewery-phone">{props.phone}</div>
-          {/* <img className="brewery_logo" src={`https://logo.clearbit.com/${props.website}?size=200`} alt="brewery logo"/> */}
-
-          <img className="brewery_logo" src={`../images/brewery_logos/${props.obdbid}_logo.png`} alt="brewery logo"/>
-          <img className="brewery_logo" src={`app/assets/images/brewery_logos/${props.id}_logo.png`} alt="brewery logo"/>
-
         </div>
       </div>
     </div>
