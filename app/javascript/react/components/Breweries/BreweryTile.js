f23189af1bb39e3aef9406d86d0e8679e551a093
luketaylor9000 <lukeandrewtaylor.lt@gmail.com>
2022-09-28 15:42:11 -0400

    method to trim review on BTiles if length > 140

--- app/javascript/react/components/Breweries/BreweryTile.js
+++ app/javascript/react/components/Breweries/BreweryTile.js
@@ -3,8 +3,19 @@ import { Link } from "react-router-dom"
 
 const BreweryTile = (props) => {
   const logo = `https://logo.clearbit.com/${props.website}?size=260`
-
   const recentReviewObject = props.reviews[(props.reviews).length-1]
+
+  const trimRecentReview = () => {
+    const recentReview = recentReviewObject.body
+    if (recentReview.length > 140) {
+      console.log('recent review is longer than 75 chars')
+      return recentReview.substring(0,140) + '...'
+    } else {
+      console.log('recent review is shorter than 75 chars')
+      return recentReview
+    }
+  }
+
   return (
     <div className="brewery-tile-container row">
       <div className="brewery-tile small-4 columns">
@@ -21,7 +32,7 @@ const BreweryTile = (props) => {
               <h3>{props.name}</h3>
               <div className="recent-review text-center">
                 <p className="recent-review-rating">{recentReviewObject.rating}</p>
-                <p className="recent-review-body">"{recentReviewObject.body}"</p> 
+                <p className="recent-review-body">"{trimRecentReview()}"</p> 
                 <p className="recent-review-username">-{recentReviewObject.username}</p>
               </div>
             </figcaption>
